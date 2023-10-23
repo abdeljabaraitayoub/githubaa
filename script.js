a350bebadacd85abc485d4e0cafddd6f7fd77c53
abdeljabaraitayoub <abdeljabarayoubi@gmail.com>
2023-10-23 11:28:31 +0100

    i added the counteurs at the 4th page

--- script.js
+++ script.js
@@ -19,5 +19,69 @@ else{
     maDiv.style.display = "block";
     
 }}
+// js
+var count1 = 0;
+
+function AddNumber1() {
+  count1++;
+  console.log("I'm clicked");
+  console.log(count1);
+
+  // Update the content of elements with class "ONE1"
+  var element = document.querySelector(".ONE1");
+    element.innerHTML = count1;
+
+}
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+var total=0;
+function AddNumber1() {
+  count1++;
+  console.log("I'm clicked");
+  console.log(count1);
+  total=total+1900;
+  console.log(total);
+
+  // Update the content of elements with class "ONE1"
+  var element = document.querySelector(".ONE1");
+    element.innerHTML = count1;
+    // Update the content of elements with class ".prrr"
+    var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+
+}
+
+function AddNumber2() {
+  count2++;
+  total=total+1900;
+  console.log(total);
+  console.log("I'm clicked");
+  console.log(count2);
+
+
+  // Update the content of elements with class "ONE2"
+  var element = document.querySelector(".ONE2");
+    element.innerHTML = count2;
+    // Update the content of elements with class ".prrr"
+    var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+
+}
+function AddNumber3() {
+  count3++;
+  console.log("I'm clicked");
+  console.log(count3);
+  total=total+1900;
+  console.log(total);
+
+  // Update the content of elements with class "ONE2"
+  var element = document.querySelector(".ONE3");
+    element.innerHTML = count3;
+
+     // Update the content of elements with class ".prrr"
+     var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+}
 
 
