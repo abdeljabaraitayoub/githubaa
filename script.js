8c9f57143da5b5cb82e268bd19bb240f8c5f306b
abdeljabaraitayoub <abdeljabarayoubi@gmail.com>
2023-10-24 14:00:01 +0100

    i added the burger nav+the toggle side bar

--- script.js
+++ script.js
@@ -19,22 +19,24 @@ else{
     maDiv.style.display = "block";
     
 }}
-// js
-var count1 = 0;
 
-function AddNumber1() {
-  count1++;
-  console.log("I'm clicked");
-  console.log(count1);
 
-  // Update the content of elements with class "ONE1"
-  var element = document.querySelector(".ONE1");
-    element.innerHTML = count1;
 
+var aside = document.querySelector("aside");
+function showSidebar() {
+if(aside.style.display === "block"){
+    aside.style.display = "none";
 }
+else{
+    aside.style.display = "block";
+    
+}}
+// js
+
 var count1 = 0;
 var count2 = 0;
 var count3 = 0;
+var count4 = 0;
 var total=0;
 function AddNumber1() {
   count1++;
@@ -43,6 +45,17 @@ function AddNumber1() {
   total=total+1900;
   console.log(total);
 
+  // Update the content of elements with class "ONE1"
+  var element = document.querySelector(".ONE1");
+    element.innerHTML = count1;
+    // Update the content of elements with class ".prrr"
+     var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+
+}
+function removeNumber1() {
+  if(count1>0){opi,
+  }
   // Update the content of elements with class "ONE1"
   var element = document.querySelector(".ONE1");
     element.innerHTML = count1;
@@ -68,6 +81,22 @@ function AddNumber2() {
     element.innerHTML = "$ " + total;
 
 }
+function removeNumber2() {
+  if(count2>0){
+  count2=count2-1;
+  console.log("I'm clicked");
+  console.log(count2);
+  total=total-1900;
+  console.log(total);
+  }
+  // Update the content of elements with class "ONE1"
+  var element = document.querySelector(".ONE2");
+    element.innerHTML = count2;
+    // Update the content of elements with class ".prrr"
+     var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+
+}
 function AddNumber3() {
   count3++;
   console.log("I'm clicked");
@@ -82,6 +111,31 @@ function AddNumber3() {
      // Update the content of elements with class ".prrr"
      var element = document.querySelector(".prrr");
     element.innerHTML = "$ " + total;
+}
+function removeNumber3() {
+  if(count3>0){
+  count3=count3-1;
+  console.log("I'm clicked");
+  console.log(count3);
+  total=total-1900;
+  console.log(total);
+  }
+  // Update the content of elements with class "ONE1"
+  var element = document.querySelector(".ONE3");
+    element.innerHTML = count3;
+    // Update the content of elements with class ".prrr"
+     var element = document.querySelector(".prrr");
+    element.innerHTML = "$ " + total;
+
+}
+
+//market counteur
+function addnumber4(){
+  count4++;
+  var market=document.querySelector("#market5");
+  market.innerHTML=count4;
+
+
 }
 
 
